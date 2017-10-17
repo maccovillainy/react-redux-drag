b6764235f9291f367b603449949a02c7aecef8a2
Your Name <you@example.com>
2017-10-17 21:26:01 +0300

    github pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './index.js',
   output: {
     filename: 'bundle.js',
-    path: './public'
+    path: __dirname + '/public'
   },
   devtool: 'source-map',
   module: {
